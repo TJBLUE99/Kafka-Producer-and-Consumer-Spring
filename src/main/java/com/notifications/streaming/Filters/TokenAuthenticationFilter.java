af6f45ceffbee915d4f2619fcf3dfc10f5d34a81
TJBLUE99 <tejaswanaik.1999@gmail.com>
2024-09-12 23:02:54 +0530

    added postgres configuration

--- src/main/java/com/notifications/streaming/Filters/TokenAuthenticationFilter.java
+++ src/main/java/com/notifications/streaming/Filters/TokenAuthenticationFilter.java
@@ -2,7 +2,6 @@ package com.notifications.streaming.Filters;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.io.IOException;
@@ -13,9 +12,9 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 
 @Component
-public class RBACFilter implements Filter {
+public class TokenAuthenticationFilter implements Filter {
 
-    Logger logger = LoggerFactory.getLogger(RBACFilter.class);
+    Logger logger = LoggerFactory.getLogger(TokenAuthenticationFilter.class);
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
@@ -23,9 +22,6 @@ public class RBACFilter implements Filter {
 
         logger.debug("Inside filter function");
 
-        // HttpRequest httprequest = (HttpRequest) servletRequest;
-
-        // logger.info("This is the request: " + httprequest.getHeaders());
         chain.doFilter(servletRequest, servletResponse);
 
     }
--- src/main/java/com/notifications/streaming/controllers/KafkaProducercController.java
+++ src/main/java/com/notifications/streaming/controllers/KafkaProducercController.java
@@ -1,49 +1,49 @@
-package com.notifications.streaming.controllers;
-
-import org.springframework.web.bind.annotation.RestController;
-
-import com.notifications.streaming.models.AdminNotification;
-import com.notifications.streaming.services.KafkaJsonProducerService;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-@CrossOrigin(origins = "*")
-@RestController
-@RequestMapping("/api")
-public class KafkaProducercController {
-
-    Logger logger = LoggerFactory.getLogger(KafkaProducercController.class);
-
-    @Autowired
-    private KafkaJsonProducerService kafkaJsonProducerService;
-
-    @PostMapping("/json")
-    @ResponseBody
-    public ResponseEntity<?> getMessage(@RequestBody AdminNotification message) {
-
-        logger.info("message: " + message.getMessage());
-        logger.info("Org name: " + message.getOrganizationName());
-        logger.info("plan name: " + message.getPlanName());
-        logger.info("status: " + message.getStatus());
-
-        if (message.getOrganizationName() == null) {
-            return new ResponseEntity<>(Map.of("message", " No data"), HttpStatus.BAD_REQUEST);
-        }
-        kafkaJsonProducerService.sendMessage(message);
-
-        return new ResponseEntity<>(Map.of("message", " Notification updated as Json"), HttpStatus.OK);
-    }
-
-}
+//package com.notifications.streaming.controllers;
+//
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.notifications.streaming.models.AdminNotification;
+////import com.notifications.streaming.services.KafkaJsonProducerService;
+//
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//import java.util.Map;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//
+//@CrossOrigin(origins = "*")
+//@RestController
+//@RequestMapping("/api")
+//public class KafkaProducercController {
+//
+//    Logger logger = LoggerFactory.getLogger(KafkaProducercController.class);
+//
+//    @Autowired
+////    private KafkaJsonProducerService kafkaJsonProducerService;
+//
+//    @PostMapping("/json")
+//    @ResponseBody
+//    public ResponseEntity<?> getMessage(@RequestBody AdminNotification message) {
+//
+//        logger.info("message: " + message.getMessage());
+//        logger.info("Org name: " + message.getOrganizationName());
+//        logger.info("plan name: " + message.getPlanName());
+//        logger.info("status: " + message.getStatus());
+//
+//        if (message.getOrganizationName() == null) {
+//            return new ResponseEntity<>(Map.of("message", " No data"), HttpStatus.BAD_REQUEST);
+//        }
+//        kafkaJsonProducerService.sendMessage(message);
+//
+//        return new ResponseEntity<>(Map.of("message", " Notification updated as Json"), HttpStatus.OK);
+//    }
+//
+//}
