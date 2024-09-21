ee0bd4f4245da6689c930033c9f7d398122bcb9e
TJBLUE99 <tejaswanaik.1999@gmail.com>
2024-09-21 16:37:43 +0530

    removed auto data configuration from application

--- src/main/java/com/notifications/streaming/StreamingApplication.java
+++ src/main/java/com/notifications/streaming/StreamingApplication.java
@@ -2,12 +2,20 @@ package com.notifications.streaming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
+
 public class StreamingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StreamingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StreamingApplication.class, args);
+    }
 
 }
